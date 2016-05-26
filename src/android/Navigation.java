a4e55bccb86a7fb4f3d54f5bade5a610ac1e3ebf
snyz.qxw <3071549858@qq.com>
2016-05-26 16:56:42 +0800

    修改导航类型
    修改导航类型为公交

--- src/android/Navigation.java
+++ src/android/Navigation.java
@@ -21,29 +21,36 @@ public class Navigation extends CordovaPlugin {
         if (action.equals("do")) {
             Intent intent = activity.getIntent();
             JSONObject jsonObject = (JSONObject)args.get(0);
-            String lat = jsonObject.getString("lat");
-            String lon = jsonObject.getString("lon");
-            String name = jsonObject.getString("name");
+            String deLat = jsonObject.getString("deLat");
+            String deLon = jsonObject.getString("deLon");
+            String deName = jsonObject.getString("deName");
+			String reLat = jsonObject.getString("reLat");
+            String reLon = jsonObject.getString("reLon");
+            String reName = jsonObject.getString("reName");
             try {
-                 //如果有百度地图
+                 //如果有百度地图 + "origin=name:我的位置&"
+				 //+ "destination=latlng:"+lat+","+lon+"|name:"+name+"&"  
+				 //
                  if(isInstallByread("com.baidu.BaiduMap")){
-                     intent = Intent.getIntent("intent://map/direction?"
-                                + "origin=latlng:"+lat+","+lon+"|name:我的位置&"
-                                + "destination="+name+""
-                                + "&mode=driving"
+                     intent = Intent.getIntent("intent://map/direction?"                                
+								//+ "origin=latlng:"+reLat+","+reLon+"|name:我的位置&"
+								+ "origin=latlng:"+reLat+","+reLon+"|name:"+reName+"&"
+								+ "destination="+deName+""  
+								//+ "destination=latlng:"+deLat+","+deLon+"&"                              
+                                + "&mode=transit"
                                 + "&referer=Autohome|GasStation#Intent;scheme=bdapp;package=com.baidu.BaiduMap;end");
                      activity.startActivities(new Intent[]{intent});//启动调用
                  }
-                 //如果有高德地图
+                 /*//如果有高德地图
                  else if(isInstallByread("com.autonavi.minimap"))
                  {
                      intent = Intent.getIntent("androidamap://navi?sourceApplication=HuoYuanTong&poiname="+name+""
                             + "&lat="+lat+"&lon="+lon+"&dev=1&style=2");
                      activity.startActivities(new Intent[]{intent});//启动调用
-                 }
+                 }*/
                  //如果没有安装导航地图
                  else{
-                     callbackContext.error("呜呜~检测到您没安装百度或高德地图,无法进行导航!");
+                     callbackContext.error("检测到您没安装百度地图,无法进行导航!");
                  }
                 } catch (Exception e) {
                      e.printStackTrace();
